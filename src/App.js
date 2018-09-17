da8af2e669e45e0e700b7f917b2f210b7dc0d29f
Jennifer Cobb <jenn.cobb.m@gmail.com>
2018-09-16 20:39:05 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ class App extends Component {
 
   clickChar = id => {
     console.log("----");
-    
+
     const match = this.state.clickedChars.filter(char => char === id);
     console.log("Match: " + match);
 
     if (match.length === 0 || this.state.clickedChars.length === 0) {
       console.log("(inside no-match)");
       var newScore = this.state.score + 1;
-      this.setState({ score: newScore});
+      this.setState({ score: newScore });
       this.checkScore();
-    
+
       // Filter this.state.chars for chars with an id equal to the id clicked
       const char = this.state.chars.filter(char => char.id === id);
       this.state.clickedChars.push(char[0].id);
@@ -34,7 +34,7 @@ class App extends Component {
       this.setState(chars);
     }
 
-    else if (match.length > 0){
+    else if (match.length > 0) {
       this.setState({ score: 0 });
 
       this.setState({ clickedChars: [] })
@@ -44,29 +44,37 @@ class App extends Component {
   };
 
   checkScore = () => {
-     if (this.state.score >= this.state.topScore){
-       this.setState({topScore: this.state.topScore + 1})
-     }
+    if (this.state.score >= this.state.topScore) {
+      this.setState({ topScore: this.state.topScore + 1 })
+    }
   }
 
   // Map over this.state.chars and render a CharCard component for each char object
   render() {
     return (
       <div>
-        <div>
-        Score: {this.state.score} | Top Score: {this.state.topScore}
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Memory Game</h1>
+            <p class="lead">Click on an image to earn points, but don't click on any more than once!</p>
+          </div>
         </div>
+        <div className="container" id = "mainSection">
+          <div id="scoreCard">
+            Score: {this.state.score} | Top Score: {this.state.topScore}
+          </div>
 
-        {this.state.chars.map(char => (
-          <CharCard
-            clickChar={this.clickChar}
-            id={char.id}
-            key={char.id}
-            image={char.image}
+          {this.state.chars.map(char => (
+            <CharCard
+              clickChar={this.clickChar}
+              id={char.id}
+              key={char.id}
+              image={char.image}
 
-          />
-        ))}
+            />
+          ))}
 
+        </div>
       </div>
     );
   }
--- src/components/CharCard/CharCard.js
+++ src/components/CharCard/CharCard.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./CharCard.css";
 
 const CharCard = props => (
-    <div className="card">
+    <div className="card" onClick={() => props.clickChar(props.id)}>
 
 
         <div className="img-container">
-            <img alt={props.name} src={props.image} onClick={() => props.clickChar(props.id)}/>
+            <img alt={props.name} src={props.image} />
 
         </div>
 
